57477b5991067e8e0d6dd90b44ada84bf2fe97c2
Oliver's probook <jennifersprobook1@Olivers-MacBook-Pro.local>
2018-01-06 18:21:35 -0500

    Modified index.php.

--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title> Simple_Spaceships! </title>
+		<title>Simple_Spaceships!</title>
 	</head>
-	<body>		
+		Username:<br>
+		<input type="text" name="Enter your username here!" value="Player_1">
+		<script> </script>
+		<br><br>
+		<input type="submit" value="Submit">
+	</form> 
+	<body>
 	</body>
 </html>
